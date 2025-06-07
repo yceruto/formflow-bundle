ea3bba6de7a63dd6456878f40ff5a5c2ee170e56
Yonel Ceruto <yonel.ceruto@scnd.com>
2025-06-07 00:03:09 -0400

    add more tests

--- tests/Integration/FormFlowBasicTest.php
+++ tests/Integration/FormFlowBasicTest.php
@@ -43,6 +43,96 @@ class FormFlowBasicTest extends AbstractWebTestCase
         self::assertSame('/basic/success', $client->getInternalResponse()->getHeader('Location'));
     }
 
+    public function testGoBackToPreviousStep(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/basic');
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step1][field11]' => 'foo',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step2][field21]' => 'foo',
+            'multistep[step2][field22]' => 'bar',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        self::assertStringContainsString('>Step3<', $crawler->html());
+
+        $crawler = $client->submit($crawler->selectButton('Back')->form(), [
+            'multistep[navigator][back]' => '',
+        ]);
+
+        self::assertStringContainsString('>Step2<', $crawler->html());
+        self::assertStringContainsString('value="foo"', $crawler->html());
+        self::assertStringContainsString('value="bar"', $crawler->html());
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step2][field21]' => 'foo',
+            'multistep[step2][field22]' => 'bar',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        $client->submit($crawler->selectButton('Finish')->form(), [
+            'multistep[step3][field31]' => 'foo',
+            'multistep[step3][field32]' => 'bar',
+            'multistep[step3][field33]' => 'baz',
+            'multistep[navigator][finish]' => '',
+        ]);
+
+        self::assertSame(302, $client->getInternalResponse()->getStatusCode());
+        self::assertSame('/basic/success', $client->getInternalResponse()->getHeader('Location'));
+    }
+
+    public function testGoBackToEarlierStep(): void
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/basic');
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step1][field11]' => 'foo',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step2][field21]' => 'foo',
+            'multistep[step2][field22]' => 'bar',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        self::assertStringContainsString('>Step3<', $crawler->html());
+
+        $crawler = $client->submit($crawler->selectButton('Back')->form(), [
+            'multistep[navigator][back]' => 'step1',
+        ]);
+
+        self::assertStringContainsString('>Step1<', $crawler->html());
+        self::assertStringContainsString('value="foo"', $crawler->html());
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step1][field11]' => 'foo',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        $crawler = $client->submit($crawler->selectButton('Next')->form(), [
+            'multistep[step2][field21]' => 'foo',
+            'multistep[step2][field22]' => 'bar',
+            'multistep[navigator][next]' => '',
+        ]);
+
+        $client->submit($crawler->selectButton('Finish')->form(), [
+            'multistep[step3][field31]' => 'foo',
+            'multistep[step3][field32]' => 'bar',
+            'multistep[step3][field33]' => 'baz',
+            'multistep[navigator][finish]' => '',
+        ]);
+
+        self::assertSame(302, $client->getInternalResponse()->getStatusCode());
+        self::assertSame('/basic/success', $client->getInternalResponse()->getHeader('Location'));
+    }
+
     private static function assertSameFileContent(string $expectedFilename, string $actualContent, bool $save = false): void
     {
         $expectedContent = self::getOutputFileContent($expectedFilename, $actualContent, $save);
